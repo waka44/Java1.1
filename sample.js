34c496a3bca32963e7e4eb85c188f723ff88ba50
EC2 Default User <sigurokaru@gmail.com>
2022-11-10 05:10:30 +0000

    2

--- sample.js
+++ sample.js
@@ -1,9 +1,5 @@
-var str = "webcamp" //グローバルスコープ
-
-function foo() {
-  console.log(str)
-  var y = "hello" //関数スコープ
+for (let i = 0; i < 10; i++) {     
+  console.log(i) 
 }
 
-foo()
-console.log(y)
+console.log(i)
