b3afba1f0444ce034be726fa08b540d878714351
EC2 Default User <sigurokaru@gmail.com>
2022-11-12 10:32:41 +0000

    3

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-// メイン部分
-let alertString;
-alertString = addString("WebCamp");
-
-// 作成した関数を呼び出す
-alert(alertString);
-
-// 作成した関数
-function addString(strA){
-  let addStr = "Hello " + strA;
-  return addStr;
-}
+let promptStr = prompt('何か好きな文字を入力してください。');
+alert(promptStr);
