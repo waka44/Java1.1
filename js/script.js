9f53a06e6794fa600461ec11f107aff919ff6f58
EC2 Default User <sigurokaru@gmail.com>
2022-11-25 03:09:19 +0000

    5

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-$('#tab-contents .tab[id != "tab1"]').hide();
+const API_KEY = "取得したAPIキー";
 
-$('#tab-menu a').on('click', function(event) {
-  $("#tab-contents .tab").hide();
-  $("#tab-menu .active").removeClass("active");
-  $(this).addClass("active");
-  $($(this).attr("href")).show();
-  event.preventDefault();
+$(function(){
+  $('#btn').on('click', function() {
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data){
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data) {
+      alert('通信に失敗しました。');      });
+  });
 });
