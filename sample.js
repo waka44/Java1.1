a3669d6c4ce55fbf970b8c65e346a76e36399a71
EC2 Default User <sigurokaru@gmail.com>
2022-11-28 04:57:57 +0000

    12

--- sample.js
+++ sample.js
@@ -1,9 +1,5 @@
-var str = "webcamp"
-
-function foo() { 
-  console.log(str)
-  var str = "dmm webcamp"
-  console.log(str)
+for (let i = 0; i < 10; i++) {     
+  console.log(i) 
 }
 
-foo()
+console.log(i)
